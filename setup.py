26466d53909c76301af800ec31940efe6362046f
Yosuke Mizutani <mogproj@gmail.com>
2024-01-07 18:48:12 -0700

    Add project description for PyPI

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup, find_packages
 
 SRC_DIR = 'src'
 
+# read the contents of the README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+
 
 def get_version():
     sys.path[:0] = [SRC_DIR]
@@ -17,6 +22,8 @@ setup(
     author_email='mogproj@gmail.com',
     license='Apache 2.0 License',
     url='https://github.com/mogproject/twwanim',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         'networkx >= 3.1',
         'manim >= 0.18.0',
